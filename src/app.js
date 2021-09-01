1053ce039d9beb15072d754a2736cc86c117b171
Martín Suárez <ms.prgramador@gmail.com>
2021-09-01 16:32:59 +0000

    los numeros de la carta tienen su color dependiendo el tipo de carta

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
+  //declaramos las variables necesarias con sus valores
   let suit = ["heart", "diamond", "club", "spade"]; // ♦ ♥ ♠ ♣
   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "king", "queen", "jack", "ace"];
 
+  //establecemos los "iconos" según el tipo de carta que caiga
   let randomSuit = suit[Math.floor(Math.random() * suit.length)];
   let icon =
     randomSuit === "heart"
@@ -22,9 +24,24 @@ window.onload = function() {
       : "♠";
   let randomNumber = number[Math.floor(Math.random() * number.length)];
 
+  //obtenemos los elementos y creamos algunos necesarios
   let card = document.querySelector(".card");
-  let suitIcon = document.createElement("i");
-  suitIcon.className = randomSuit; //randomSuit
-  suitIcon.innerHTML = icon;
-  card.appendChild(suitIcon);
+  let suitSpan = document.createElement("span");
+  let numberSpan = document.createElement("span");
+
+  //le asignamos como clase el nombre del tipo de carta (diamond, heart, club, spade)
+  suitSpan.className = randomSuit;
+
+  //le pasamos como elemento o texto el ícono que corresponde según el tipo de carta
+  suitSpan.innerHTML = icon;
+
+  //según el tipo de carta es el color del número que se muestra en la carta (si es diamante o corazón los colores son en rojo)
+  randomSuit === "heart" || randomSuit === "diamond"
+    ? (numberSpan.className = "numberCardRed")
+    : (numberSpan.className = "numberCardBlack");
+  numberSpan.innerHTML = randomNumber; //aquí le pasamos como valor el numero random a la etiqueta span
+
+  //se las agregamos a la etiqueta div.card
+  card.appendChild(suitSpan);
+  card.appendChild(numberSpan);
 };
