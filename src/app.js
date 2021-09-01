146c8f8d8d778ba48c1921dfe45e0144d98dfb48
Martín Suárez <ms.prgramador@gmail.com>
2021-09-01 03:25:52 +0000

    random icons for each suit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let suit = ["heart", "diamond", "club", "spade"]; // ♦ ♥ ♠ ♣
+  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "king", "queen", "jack", "ace"];
+
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let icon =
+    randomSuit === "heart"
+      ? "♥"
+      : randomSuit === "diamond"
+      ? "♦"
+      : randomSuit === "club"
+      ? "♣"
+      : "♠";
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+  let card = document.querySelector(".card");
+  let suitIcon = document.createElement("i");
+  suitIcon.className = randomSuit; //randomSuit
+  suitIcon.innerHTML = icon;
+  card.appendChild(suitIcon);
 };
